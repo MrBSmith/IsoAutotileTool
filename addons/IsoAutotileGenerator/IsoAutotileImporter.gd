ff485758b014f813031c2b08b2864279c9dbada4
Hugo Baba <Hugobarette@gmail.com>
2021-06-08 21:17:49 +0200

    Edit tileset & fetch function refacto

--- addons/IsoAutotileGenerator/IsoAutotileImporter.gd
+++ addons/IsoAutotileGenerator/IsoAutotileImporter.gd
@@ -5,6 +5,14 @@ class_name IsoAutotileImporter
 onready var editor_interface = get_editor_interface()
 onready var editor_file_system = editor_interface.get_resource_filesystem()
 
+onready var file_dialogue_scene = load("res://addons/IsoAutotileGenerator/FileDialogue/FileDialog.tscn")
+var tileset_template_path = "res://addons/IsoAutotileGenerator/TilesetTemplate/TilesetTemplate.tres" 
+
+enum SUB_PART{
+	CORNER,
+	SIDE
+}
+
 var tile_size := Vector2(32, 16)
 var autotile_nb_tiles := Vector2(5, 10)
 var empty_tile_pos := Vector2(128, 0)
@@ -22,7 +30,8 @@ var last_path : String = ""
 
 var handled_ext = ["png", "jpg", "jpeg"]
 
-var button : Button = null
+var file_dialogue : FileDialog = null
+var button_dict : Dictionary = {}
 
 var print_logs : bool = true
 
@@ -45,39 +54,35 @@ func _process(delta: float) -> void:
 		last_path = path
 
 
-
 func is_file(path: String) -> bool:
 	var splitted_path = path.split("/")
 	var last_elem = splitted_path[-1]
-	var is_file = ".".is_subsequence_ofi(last_elem)
-	
-	if print_logs:
-		if is_file:
-			print("The given path : %s is a file" % path)
-		else:
-			print("The given path : %s is not a file" % path)
-	return is_file
+	return ".".is_subsequence_ofi(last_elem)
 
 
 func is_file_type_handled(path) -> bool:
 	var splitted_path = path.split(".")
 	var last_elem = splitted_path[-1]
-	var is_file_handled = last_elem in handled_ext
-	
-	if print_logs:
-		if is_file_handled:
-			print("The given file : %s is handled" % path)
-		else:
-			print("The given file : %s is not handled" % path)
-	return is_file_handled
+	return last_elem in handled_ext
+
+
+func is_file_tileset(path: String) -> bool:
+	var splitted_path = path.split(".")
+	var last_elem = splitted_path[-1]
+	if last_elem == "tres":
+		var resource = load(path)
+		return resource is TileSet
+	return false
 
 
 func create_button(button_name: String):
-	if is_instance_valid(button):
+	var min_button_name = button_name.to_lower().replace(" ", "_")
+	if button_dict.get(min_button_name) != null && \
+		is_instance_valid(button_dict[min_button_name]):
 		return
 	
-	button = Button.new()
-	var min_button_name = button_name.to_lower().replace(" ", "_")
+	var button = Button.new()
+	button_dict[min_button_name] = button
 	button.name = min_button_name
 	button.text = button_name
 	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, button)
@@ -87,10 +92,16 @@ func create_button(button_name: String):
 	button.connect("pressed", self, "_on_button_%s_pressed" % min_button_name)
 
 
-func derstroy_button(button_name: String) -> void:
-	if is_instance_valid(button):
-		remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, button)
+func destroy_button(button_name: String) -> void:
+	var min_button_name = button_name.to_lower().replace(" ", "_")
+	var button = button_dict.get(min_button_name)
+	if button != null && is_instance_valid(button):
+		remove_control_from_container(CONTAINER_CANVAS_EDITOR_MENU, button)
 		button.queue_free()
+		button_dict.erase(min_button_name)
+		
+		if print_logs:
+			print("button destroyed: %s" % min_button_name)
 
 
 #### VIRTUALS ####
@@ -106,8 +117,8 @@ func generate_autotile(file_path: String, output_path: String) -> void:
 	var src_file_name = get_file_name(file_path)
 
 	# Fetch the rect of the sides and
-	sides_rect_array = _fetch_sides()
-	corner_rect_array = _fetch_corners()
+	sides_rect_array = _fetch_subtexture_rect(SUB_PART.SIDE)
+	corner_rect_array = _fetch_subtexture_rect(SUB_PART.CORNER)
 
 	output_img = Image.new()
 	var output_size = tile_size * autotile_nb_tiles
@@ -115,47 +126,45 @@ func generate_autotile(file_path: String, output_path: String) -> void:
 	# Copie the source image
 	output_img.create(output_size.x, output_size.y, false, Image.FORMAT_RGBA8)
 	output_img.blit_rect(src_img, src_image_rect, Vector2.ZERO)
+	
+	_place_base_tile(tile_size * Vector2(0, 2), Vector2(5, 8))
 
 	# Place 2 sides tiles
-	_place_base_tile(tile_size * Vector2(0, 2), Vector2(2, 2))
 	_place_sides(tile_size * Vector2(0, 2), Vector2(2, 2), 2)
 
 	# Place 3 sides tiles
-	_place_base_tile(tile_size * Vector2(2, 2), Vector2(2, 2))
 	_place_sides(tile_size * Vector2(2, 2), Vector2(2, 2), 3)
 
 	# Place 2 sides and 1 corner tiles
-	_place_base_tile(tile_size * Vector2(0, 4), Vector2(2, 2))
 	_place_sides(tile_size * Vector2(0, 4), Vector2(2, 2), 2)
 	_place_corners(tile_size * Vector2(0, 4), Vector2(2, 2), [1, 3, 0, 2])
 
 	# Place the 4 sides tile
-	_place_base_tile(tile_size * Vector2(4, 1), Vector2(1, 1))
 	_place_sides(tile_size * Vector2(4, 1), Vector2(1, 1), 4)
 
 	# Place the 2 sides parallel tile
-	_place_base_tile(tile_size * Vector2(4, 2), Vector2(1, 2))
 	_place_sides(tile_size * Vector2(4, 2), Vector2(1, 2), 2, true)
 
 	# Place the 2 corners sides
-	_place_base_tile(tile_size * Vector2(2, 4), Vector2(3, 2))
 	_place_corners(tile_size * Vector2(2, 4), Vector2(3, 2), [0, 2, 1, 3, 2, 0])
 	_place_corners(tile_size * Vector2(2, 4), Vector2(3, 2), [2, 1, 3, 0, 3, 1])
 
 	# Place the 1 side 1 corner
-	_place_base_tile(tile_size * Vector2(0, 6), Vector2(4, 2))
 	_place_sides(tile_size * Vector2(0, 6), Vector2(4, 2), 1)
 	_place_corners(tile_size * Vector2(0, 6), Vector2(2, 2), [1, 0, 3, 3])
 	_place_corners(tile_size * Vector2(2, 6), Vector2(2, 2), [0, 1, 2, 2])
-
+	
+	# Place the 1 Side 2 corners
+	_place_sides(tile_size * Vector2(0, 8), Vector2(4, 1), 1)
+	_place_corners(tile_size * Vector2(0, 8), Vector2(4, 1), [1, 0, 0, 1])
+	_place_corners(tile_size * Vector2(0, 8), Vector2(4, 1), [3, 3, 2, 2])
+	
 	# Place the 3 corners tiles
-	_place_base_tile(tile_size * Vector2(0, 8), Vector2(4, 1))
-	_place_corners(tile_size * Vector2(0, 8), Vector2(4, 1), [0, 1, 2, 3])
-	_place_corners(tile_size * Vector2(0, 8), Vector2(4, 1), [1, 2, 3, 0])
-	_place_corners(tile_size * Vector2(0, 8), Vector2(4, 1), [2, 3, 0, 1])
+	_place_corners(tile_size * Vector2(0, 9), Vector2(4, 1), [0, 1, 2, 3])
+	_place_corners(tile_size * Vector2(0, 9), Vector2(4, 1), [1, 2, 3, 0])
+	_place_corners(tile_size * Vector2(0, 9), Vector2(4, 1), [2, 3, 0, 1])
 
 	# place the 4 corners tiles
-	_place_base_tile(tile_size * Vector2(4, 6), Vector2(1, 1))
 	for i in range(4):
 		_place_corners(tile_size * Vector2(4, 6), Vector2(1, 1), [i])
 
@@ -178,7 +187,7 @@ func _place_sides(origin: Vector2, nb_tiles: Vector2, nb_sides: int, opposite: b
 				var side_id = wrapi(iter - k - (k * int(opposite)), 0, 4)
 				var side_rect = sides_rect_array[side_id]
 				var src_cell = _find_cell(side_id)
-				var inside_cell_pos = _find_side_inside_cell_pos(src_cell)
+				var inside_cell_pos = _find_inside_cell_pos(SUB_PART.SIDE, src_cell)
 				var cell_pos = Vector2(j, i) * tile_size
 				var dest_pos = origin + cell_pos + inside_cell_pos
 				output_img.blit_rect(src_img, side_rect, dest_pos)
@@ -194,7 +203,7 @@ func _place_corners(origin: Vector2, nb_tiles:= Vector2(2, 2), corners_id_array 
 			var corner_rect = corner_rect_array[corner_id]
 			var cell = Vector2(j, i)
 			var corner_cell = _find_cell(corner_id)
-			var inside_cell_pos = _find_corner_inside_cell_pos(corner_cell)
+			var inside_cell_pos = _find_inside_cell_pos(SUB_PART.CORNER, corner_cell)
 			var dest_pos = origin + (cell * tile_size) + inside_cell_pos
 			output_img.blit_rect(src_img, corner_rect, dest_pos)
 			iter += 1
@@ -212,40 +221,57 @@ func _find_line(id: int) -> int:
 	return int(float(id) / 2)
 
 
-func _find_side_inside_cell_pos(cell_pos: Vector2) -> Vector2:
-	var x = int(cell_pos.x) if cell_pos.y == 0 else int(!bool(cell_pos.x))
-	return half_tile * Vector2(x, cell_pos.y)
-
-
-func _find_corner_inside_cell_pos(cell_pos: Vector2) -> Vector2:
-	if cell_pos.y == 0:
-		return Vector2(cell_pos.x * (tile_size.x - quarter_tile.x),  half_tile.y - quarter_tile.y / 2)
+func _find_inside_cell_pos(sub_part_type: int, cell_pos: Vector2) -> Vector2:
+	if sub_part_type == SUB_PART.SIDE:
+		var x = int(cell_pos.x) if cell_pos.y == 0 else int(!bool(cell_pos.x))
+		return half_tile * Vector2(x, cell_pos.y)
 	else:
-		return Vector2(half_tile.x - quarter_tile.x / 2, cell_pos.x * (tile_size.y - quarter_tile.y))
+		if cell_pos.y == 0:
+			return Vector2(cell_pos.x * (tile_size.x - quarter_tile.x),  half_tile.y - quarter_tile.y / 2)
+		else:
+			return Vector2(half_tile.x - quarter_tile.x / 2, cell_pos.x * (tile_size.y - quarter_tile.y))
 
 
-func _fetch_sides() -> Array:
-	var sides_array = []
+func add_autotile_to_tileset(tileset_path: String, texture_path: String):
+	var tileset = load(tileset_path)
+	if print_logs && tileset != null:
+		print("Tileset loaded from path %s" % tileset_path)
+		
+	var texture = load(texture_path)
+	if print_logs && texture != null:
+		print("Texture loaded from path %s" % texture_path)
+		
+	var tile_id = tileset.get_tiles_ids().size()
+	tileset.create_tile(tile_id)
+	tileset.tile_set_texture(tile_id, texture)
+	ResourceSaver.save(last_path, tileset)
+
+
+func _fetch_subtexture_rect(sub_part_type: int) -> Array:
+	var origin = Vector2(2 * tile_size.x, 0) if sub_part_type == SUB_PART.CORNER else Vector2.ZERO
+	var subtexture_size = half_tile if sub_part_type == SUB_PART.SIDE else quarter_tile
+	var part_array = []
 	for i in range(2):
 		for j in range(2):
 			var cell_pos = Vector2(j, i)
 			var tile_pos = tile_size * cell_pos
-			var inside_tile_pos = _find_side_inside_cell_pos(cell_pos)
-			var rect = Rect2(tile_pos + inside_tile_pos, half_tile)
-			sides_array.append(rect)
-	return sides_array
+			var inside_tile_pos = _find_inside_cell_pos(sub_part_type, cell_pos)
+			var rect = Rect2(origin + tile_pos + inside_tile_pos, subtexture_size)
+			part_array.append(rect)
+	return part_array
 
 
-func _fetch_corners() -> Array:
-	var sides_array = []
-	var corner_origin = Vector2(2 * tile_size.x, 0)
-	for i in range(2):
-		for j in range(2):
-			var cell = Vector2(j, i)
-			var inside_cell_pos = _find_corner_inside_cell_pos(cell)
-			var rect = Rect2(corner_origin + inside_cell_pos + cell * tile_size, quarter_tile)
-			sides_array.append(rect)
-	return sides_array
+#func _fetch_corners() -> Array:
+#	var sides_array = []
+#	var corner_origin = Vector2(2 * tile_size.x, 0)
+#	for i in range(2):
+#		for j in range(2):
+#			var cell = Vector2(j, i)
+#			var cell_pos = cell * tile_size
+#			var inside_cell_pos = _find_corner_inside_cell_pos(cell)
+#			var rect = Rect2(corner_origin + inside_cell_pos + cell_pos, quarter_tile)
+#			sides_array.append(rect)
+#	return sides_array
 
 
 func get_file_name(path: String) -> String:
@@ -267,11 +293,38 @@ func _on_selected_path_changed(path: String) -> void:
 	if is_file(path) && is_file_type_handled(path):
 		create_button("Generate autotile")
 	else:
-		derstroy_button("Generate autotile")
+		destroy_button("Generate autotile")
+	
+	if is_file(path) && is_file_tileset(path):
+		print("Tileset handled")
+		create_button("Add autotile texture")
+	else:
+		destroy_button("Add autotile texture")
 
 
 func _on_button_generate_autotile_pressed() -> void:
 	if print_logs:
-		print("Autotile gen pressed")
+		print("Autotile gen button pressed")
 	
 	generate_autotile(last_path, last_path.get_base_dir() + "/")
+
+
+func _on_button_add_autotile_texture_pressed() -> void:
+	if print_logs:
+		print("Add autotile texture button pressed")
+		
+		file_dialogue = file_dialogue_scene.instance()
+		file_dialogue.connect("file_selected", self, "_on_file_dialogue_file_selected")
+		
+		editor_interface.get_editor_viewport().add_child(file_dialogue)
+		file_dialogue.set_current_dir(last_path.get_base_dir() + "/")
+		file_dialogue.set_current_path(last_path.get_base_dir() + "/")
+		file_dialogue.set_visible(true)
+		file_dialogue.invalidate()
+
+
+func _on_file_dialogue_file_selected(texture_file_path: String) -> void:
+	if is_file(texture_file_path) && is_file_type_handled(texture_file_path):
+		file_dialogue.queue_free()
+		
+		add_autotile_to_tileset(last_path, texture_file_path)
